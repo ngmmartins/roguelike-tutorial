749c841ea6d9f87bef075932db3c38a45ae1e3be
Nuno Martins <nmartins10@gmail.com>
2021-04-07 10:57:47 +0100

    ch04-newmap

---
+++ ch04-newmap/src/main.rs
@@ -0,0 +1,69 @@
+use rltk::{GameState, Rltk, RGB};
+use specs::prelude::*;
+
+mod components;
+mod map;
+mod player;
+mod rect;
+
+pub use components::*;
+pub use map::*;
+pub use player::*;
+pub use rect::*;
+
+pub struct State {
+    pub ecs: World,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+
+    let mut gs = State { ecs: World::new() };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+
+    let (player_x, player_y) = rooms[0].center();
+
+    gs.ecs
+        .create_entity()
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player {})
+        .build();
+
+    rltk::main_loop(context, gs)
+}
